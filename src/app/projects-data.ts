1a8c1c9c3c59b9e50acfe9593fcc66146b07c08b
Saidfatah <said_designer@outlook.com>
2026-08-14 17:26:16 +0100

    created projects-data.ts

---
+++ src/app/projects-data.ts
@@ -0,0 +1,63 @@
+const projects = [
+    {
+        id:"wax-poetics",
+        headingTitle:"Wax Poetics",
+        title:"Wax Poetics",
+        client:"Wonderland",
+        year:"2020"
+    },
+    {
+        id:"nike-virtual-closet",
+        headingTitle:"Nike",
+        title:"Nike Virtual Closet",
+        client:"Rehab Agency",
+        year:"2021"
+    },
+    {
+        id:"carnival-sounds",
+        headingTitle:"Spotify",
+        title:"Carnival Sounds",
+        hasDecsenders:true,
+        client:"Stink Studios",
+        year:"2020"
+    },
+    {
+        id:"cowboy",
+        headingTitle:"Cowboy",
+        title:"Cowboy",
+        client:"Cowboy",
+        hasDecsenders:true,
+        year:"2021"
+    },
+    {
+        id:"various",
+        headingTitle:"Apple",
+        title:"Various",
+        client:"Apple / Engine",
+        hasDecsenders:true,
+        year:"2022"
+    },
+    {
+        id:"bolt",
+        headingTitle:"Bolt",
+        title:"Bolt",
+        client:"Studio Koto",
+        year:"2022"
+    },
+    {
+        id:"twentynine-palms",
+        headingTitle:"29 Palms",
+        title:"Twentynine Palms",
+        client:"Independent",
+        year:"2022"
+    },
+    {
+        id:"wework-with-you",
+        headingTitle:"WeWork",
+        title:"WeWork with you",
+        client:"Stink Studios",
+        year:"2019"
+    }
+]
+
+export default projects
