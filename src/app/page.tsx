c74f66426de82dd30d20bc9f511d979b098481f6
Saidfatah <said_designer@outlook.com>
2026-08-15 23:34:49 +0100

    use useVirtualScroll for projects container and handle hiding custom cursor and project info and blocking pointer events while scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import { useLayoutEffect, useRef, useState } from "react";
 import CustomCursor from "./components/CustomCursor";
 import ProjectsHeadingsShowcase from "./components/ProjectsHeadingsShowcase";
 import ProjectsInfo from "./components/ProjectsInfo";
+import { useVirtualScroll } from "./hooks/useVirtualScroll";
+import { CustomEventsMap, CustomEventsPayloads } from "./types/events.types";
 
 const Layout = ({ children }: { children: (layoutReady: boolean) => React.ReactNode }) => {
   const rootDiv = useRef<HTMLDivElement>(null);
   const [layoutReady, setLayoutReady] = useState(false);
+
 
   useLayoutEffect(() => {
     if (!rootDiv.current) return;
@@ -55,13 +58,41 @@ const Layout = ({ children }: { children: (layoutReady: boolean) => React.ReactN
 };
 
 export default function Home() {
+  const scrollAbleContainer = useRef<HTMLDivElement>(null);
+  const scrollPointerRef = useRef<HTMLDivElement>(null);
+
+  useVirtualScroll(scrollAbleContainer, {
+    scrollPointerRef, onScrollStart: () => {
+      window.dispatchEvent(
+        new CustomEvent<CustomEventsPayloads["projectinfo:hide"]>(CustomEventsMap["projectinfo:hide"], {
+          detail: {
+            projectId: "",
+          },
+        })
+      );
+      window.dispatchEvent(
+        new CustomEvent<CustomEventsPayloads["cursor:update"]>(CustomEventsMap["cursor:update"], {
+          detail: {
+            show: false,
+            pointer: false,
+          },
+        })
+      );
+    }
+  });
+
   return (
     <Layout>
       {(layoutReady: boolean) => (
         <main id="main">
           <CustomCursor />
+          <div
+            ref={scrollPointerRef}
+            id="scroll-pointer"
+          />
 
           <div
+            ref={scrollAbleContainer}
             className={`container ${layoutReady ? "row" : ""} flex-end`}
           >
             <div className="column-8">
